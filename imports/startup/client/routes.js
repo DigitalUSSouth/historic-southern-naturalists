f60e599f3beadf847bf8284144b86e7d38b7c809
Collin Haines <collinhaines@me.com>
2016-09-01 16:28:00 -0400

    Added More Routes

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,11 +1,31 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-import '/imports/components/video.js';
+// Import layout.
+import '/imports/ui/layouts/scaffolding.js';
+
+// Import component.
+import '/imports/ui/components/navigation.js';
+
+// Import pages.
+import '/imports/ui/pages/home.js';
+import '/imports/ui/pages/timeline.js';
+import '/imports/ui/pages/video.js';
 
 FlowRouter.route('/', {
-  name: 'home',
   action() {
-    BlazeLayout.render('video');
+    BlazeLayout.render('scaffolding', { main: 'home' });
+  }
+});
+
+FlowRouter.route('/timeline', {
+  action() {
+    BlazeLayout.render('scaffolding', { main: 'timeline' });
+  }
+});
+
+FlowRouter.route('/video', {
+  action() {
+    BlazeLayout.render('scaffolding', { main: 'video' });
   }
 });
