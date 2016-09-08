1285c142c7e9511125b2a7e5ac9f7834b8222802
Collin Haines <collinhaines@me.com>
2016-09-08 15:44:30 -0400

    Created Timeline Content

--- imports/ui/pages/timeline.js
+++ imports/ui/pages/timeline.js
@@ -1,3 +1,15 @@
 import './timeline.html';
 
-// http://timeline.knightlab.com/ => https://atmospherejs.com/agnito/timeline
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
+Template.timeline.onRendered(function timelineRendered() {
+  // JSON Docs: https://timeline.knightlab.com/docs/json-format.html
+  createStoryJS({
+    type:     'timeline',
+    width:    '100%',
+    height:   '600',
+    source:   '/timeline.json',
+    embed_id: 'timeline'
+  });
+});
