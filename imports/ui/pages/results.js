fd700d1cc832ae44a5264e5372bc8c8c3be20541
Collin Haines <collinhaines@me.com>
2016-09-23 13:30:14 -0400

    Automatic Re-searching
    The results table will now change after a user types in the search
    query a second time without a full-page reload.

--- imports/ui/pages/results.js
+++ imports/ui/pages/results.js
@@ -3,17 +3,20 @@ import './results.html';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
+import { Tracker } from 'meteor/tracker';
 import { Meteor } from 'meteor/meteor';
 
 Template.results.onCreated(function () {
   Template.instance().results = new ReactiveVar(false);
 
-  Meteor.call('search', FlowRouter.getParam('query'), (error, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      this.results.set(result);
-    }
+  Tracker.autorun(() => {
+    Meteor.call('search', FlowRouter.getParam('query'), (error, result) => {
+      if (error) {
+        console.log(error);
+      } else {
+        this.results.set(result);
+      }
+    });
   });
 });
 
