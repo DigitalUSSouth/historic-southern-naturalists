63cfddf33c4fd3daff956bf88056134f6b14fe8a
Collin Haines <collinhaines@me.com>
2016-09-29 17:00:08 -0400

    Specify Object Result

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -17,7 +17,7 @@ Meteor.methods({
   'search'(query) {
     check(query, String);
 
-    return Meteor.call('q', 'dmQuery/hsn/CISOSEARCHALL^' + query + '^any/title!descri!contri/0/1024/0/0/0/0/0/1/json');
+    return Meteor.call('q', 'dmQuery/hsn/CISOSEARCHALL^' + query + '^any/title!descri!contri/0/1024/0/0/0/0/0/1/json').data.records;
   },
 
   /**
