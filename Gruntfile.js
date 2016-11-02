dbd30575dc1e134a3fd56bfe8e52eaaab318cf78
Collin Haines <collinhaines@me.com>
2016-11-02 11:12:32 -0400

    Update Development Command

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,19 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    browserSync: {
+      default: {
+        options: {
+          open:      false,
+          proxy:     'localhost:8000',
+          notify:    true,
+          watchTask: true
+        }
+      }
+    },
+
     concat: {
       default: {
-        src: ['js/bootstrap/*.js'],
+        src:  ['js/bootstrap/*.js'],
         dest: 'js/bootstrap-3.3.7.min.js'
       }
     },
@@ -18,11 +29,31 @@ module.exports = function (grunt) {
           'css/font-awesome-4.6.3.min.css': 'css/font-awesome/font-awesome.less'
         }
       }
+    },
+
+    php: {
+      default: { }
+    },
+
+    watch: {
+      less: {
+        files: ['css/**/*.less'],
+        tasks: ['less']
+      },
+
+      scripts: {
+        files: '<%= concat.default.src %>',
+        tasks: ['concat']
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-php');
 
+  grunt.registerTask('dev', ['php', 'browserSync', 'watch']);
   grunt.registerTask('default', ['concat', 'less']);
 };
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Historic Southern Naturalists
 
 ## Development
-Simply, a one-time usage of `npm install` followed by `grunt` will compile and concatenate files responsibly.
+### Pre-Requisites
+#### Install Node.js and NPM
+- [Linux Installation](http://blog.teamtreehouse.com/install-node-js-npm-linux)
+- [Mac Installation](http://blog.teamtreehouse.com/install-node-js-npm-mac)
+- [Windows Installation](http://blog.teamtreehouse.com/install-node-js-npm-windows)
 
-Execute `grunt` after any changes are done within `src/**/*` or `js/**/*`. Alternatively, `grunt less` and `grunt concat` will be more specific, in order to save time on execution.
+### Install Dependencies
+Navigate to the project root directory and execute `npm install`.
+
+## Active
+When developing on this project, utilizing the command `grunt dev` will open up a PHP server on port 3000.
