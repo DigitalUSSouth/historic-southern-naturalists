0cb42838514f3c19f71b82fe54718d26d448b833
Collin Haines <collinhaines@me.com>
2016-11-04 12:35:36 -0400

    Created Timeline Page

---
+++ timeline.php
@@ -0,0 +1,18 @@
+<?php
+require "includes/application.php";
+
+$application->setTitle("Timeline");
+
+require "includes/header.php";
+?>
+<div class="row">
+  <div class="col-xs-12">
+    <h1>Timeline</h1>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-xs-12">
+    <iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1suOy5pdYL1R6eD9ssildwnSdxXoFSFda8wy2uRkIlQc&font=Default&lang=en&initial_zoom=2&height=650" width="100%" height="650" frameborder="0"></iframe>
+  </div>
+</div>
