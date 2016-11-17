1e6830a0e9c0f7c75ab374bbfb9de0efd191ab71
Collin Haines <collinhaines@me.com>
2016-11-17 17:06:29 -0500

    Links for <script> and <link> Regarding BookReader

--- includes/application.php
+++ includes/application.php
@@ -6,6 +6,7 @@ class Application {
   private $url;
   private $title;
   private $connection;
+  private $isManuscriptViewer;
 
   /**
    * Constructor
@@ -18,6 +19,8 @@ class Application {
     $this->connection = new PDO("pgsql:" . $contents["connection"], $contents["username"], $contents["password"]);
 
     $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $this->isManuscriptViewer = false;
   }
 
   /**
@@ -62,6 +65,11 @@ class Application {
   public function renderCSS() {
     $html  = "";
     $files = array("bootstrap-3.3.7.min.css", "font-awesome-4.6.3.min.css");
+
+    // Append files that need to be rendered for manuscript-viewer.php.
+    if ($this->isManuscriptViewer) {
+      array_push($files, "BookReader.css");
+    }
 
     foreach ($files as $file) {
       $html .= '<link rel="stylesheet" href="' . $this->url . 'css/' . $file . '">';
@@ -110,7 +118,36 @@ class Application {
    */
   public function renderScripts() {
     $html  = "";
-    $files = array("jquery-3.1.1.min.js", "bootstrap-3.3.7.min.js", "dataTables-1.10.12.min.js", "dataTables-bootstrap-1.10.12.min.js", "hsn.js");
+    $files = array(
+      "jquery-3.1.1.min.js",
+      "bootstrap-3.3.7.min.js",
+      "dataTables-1.10.12.min.js",
+      "dataTables-bootstrap-1.10.12.min.js",
+      "hsn.js"
+    );
+
+    // Append files that need to be rendered for manuscript-viewer.php.
+    if ($this->isManuscriptViewer) {
+      $books = array(
+        "jquery-ui-1.12.0.min.js",
+        "jquery.browser.min.js",
+        "dragscrollable.js",
+        "jquery.colorbox-min.js",
+        "jquery.ui.ipad.js",
+        "jquery.bt.min.js",
+        "BookReader.js",
+      );
+
+      foreach ($books as $file) {
+        if (pathinfo($file)["extension"] !== "js") {
+          continue;
+        }
+
+        array_push($files, 'bookreader/' . $file);
+      }
+
+      array_push($files, 'hsn-book-reader.js');
+    }
 
     foreach ($files as $file) {
       $html .= '<script src="' . $this->url . 'js/' . $file . '"></script>';
@@ -137,6 +174,10 @@ class Application {
   /**
    * Mutators
    */
+  public function setIsManuscriptViewer($isManuscriptViewer) {
+    $this->isManuscriptViewer = $isManuscriptViewer;
+  }
+
   public function setTitle($title) {
     $this->title = $title;
   }
