dca8e3cbebcfc9bd138b5978e8c6f2c65f686f61
Tyler Moon <moon.tyler@gmail.com>
2017-01-20 11:35:01 -0500

    Fixed the scroll point and made the animation smooth in the Video page

--- js/hsn.js
+++ js/hsn.js
@@ -7,8 +7,8 @@
 // Assign all DataTables.
 $('[data-plugin="dataTable"]').dataTable();
 
+// Code for the video player on the Video page
 $(document).ready(function () {
-  // Code for the video player on the Video page
   // Get all the thumbnail elements
   var videos = document.querySelectorAll(".thumbnail");
   for(var i = 0; i < videos.length; i++){
@@ -18,30 +18,27 @@ $(document).ready(function () {
   // Click listener
   function clickHandler(el){
 
-    var scrollHere = "<div id=\"scroll-here\">"
-    var videoPlayer = "<div id=\"jumbotron\"><div class=\"row\"></div><div class=\"col-xs-12\" style=\"margin:0px;padding:0px;\"><div  class=\"jumbotron\" style=\"margin:0px;padding:0px;border-style:solid;\"><div class=\"embed-responsive embed-responsive-4by3\"><video id=\"mainVideo\" autoplay controls style=\"margin-top:0px;\"></video><h3 class=\"text-center\" style=\"margin-top:30%\">Click on a video below to view</h3></div></div></div></div><br /><div class=\"row\"><div class=\"col-xs-12\"> </div>";
-
-
-
+    // HTML for the vido player element to be added in
+    var videoPlayer = "<div id=\"scroll-here\"><div id=\"jumbotron\"><div class=\"row\"></div><div class=\"col-xs-12\" style=\"margin:0px;padding:0px;\"><div  class=\"jumbotron\" style=\"margin:0px;padding:0px;border-style:solid;\"><div class=\"embed-responsive embed-responsive-4by3\"><video id=\"mainVideo\" autoplay controls style=\"margin-top:0px;\"></video><h3 class=\"text-center\" style=\"margin-top:30%\">Click on a video below to view</h3></div></div></div></div><br /><div class=\"row\"><div class=\"col-xs-12\"> </div>";
     console.log(el.srcElement.parentElement.parentElement);
-
     var jumbotron = document.getElementById("jumbotron");
+    // The big video player has not be created so create one
     if(jumbotron === null){
-      // The big video player has not be created so create one
       console.log("Creating jumbotron");
       console.log(el.srcElement);
-      // Create the scroll point to scroll the view to the main video but put it above the thumbnail to not confuse the user
-      el.srcElement.parentElement.parentElement.insertAdjacentHTML('beforebegin',scrollHere)
-
       // Create the main video player below the thumbnail
       el.srcElement.parentElement.parentElement.insertAdjacentHTML('afterend',videoPlayer);
-
       // Get the mainVideo element to display the video in
       var mainVideo = document.getElementById("mainVideo");
       mainVideo.src = el.srcElement.currentSrc;
       // Scroll up to view the mainVideo
-      var scrollHere = document.getElementById("scroll-here")
-      scrollHere.scrollIntoView({ behavior: 'smooth'});
+    //  var scrollHere = document.getElementById("scroll-here")
+
+      $('html, body').animate({
+        scrollTop: $("#scroll-here").offset().top
+      },1000);
+
+      //scrollHere.scrollIntoView({ behavior: 'smooth'});
     }else{
       // The big video player has been created so destroy it
       console.log("Destroying jumbotron");
