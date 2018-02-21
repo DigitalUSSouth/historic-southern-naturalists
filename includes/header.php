1817789db977a40cbccc68bbb5f19c46a09362e1
nikhila <nikhilacheepurupalli@gmail.com>
2018-02-21 11:15:08 -0500

    Major CSS changes; linked the tabs to the correct links

--- includes/header.php
+++ includes/header.php
@@ -34,9 +34,17 @@ $navigation = array(
   <link href="css/videojs-playlist-ui.vertical.css" rel="stylesheet">-->
 
 </head>
-<body>
+<style>
+  body {
+    background-color: #d3bf96;
+    color: black; 
+    font-size: 15px;
+    margin-top: 50px; 
+  }
+</style>
+<body >
   <?php if (!$application->isManuscriptViewer()): ?>
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: #9c6409;">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -46,17 +54,45 @@ $navigation = array(
             <span class="icon-bar"></span>
           </button>
 
-          <a href="<?php print $application->getURL(); ?>" class="navbar-brand">Historic Southern Naturalists</a>
+          <a href="<?php print $application->getURL(); ?>" class="navbar-brand"><text style="color: #3cb6ce; align: center;">Historic Southern Naturalists</text></a>
+          <img src="img/filmstrip.png" alt="filmstrip" style="width: 400px; height: 100px; align: middle">
         </div>
 
-        <div class="collapse navbar-collapse" id="navbar">
-          <ul class="nav navbar-nav navbar-right">
-            <?php foreach ($navigation as $item): ?>
+        <br><br>
+        <div class="collapse navbar-collapse" id="navbar" style="align: center">
+          <ul class="nav navbar-nav navbar-right nav-pills" style="text-align: center;">
+            <li class="nav-item">
+               <a class="nav-link active" href="<?php print $application->getURL(); ?>" style="color: #3cb6ce">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/#about-naturalist" style="color: #3cb6ce">About the Naturalists</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/#about-project" style="color: #3cb6ce">About the Project</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/browse-all.php" style="color: #3cb6ce">Browse All</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/search.php" style="color: #3cb6ce">Search</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/gallery.php" style="color: #3cb6ce">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/timeline.php" style="color: #3cb6ce">Timeline</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hsn/videos.php" style="color: #3cb6ce">Videos</a>
+            </li>
+
+
+            <?php /*foreach ($navigation as $item): ?>
               <?php $link = $item === 'Home' ? '' : str_replace(' ', '-', strtolower($item)) . ".php"; ?>
               <li>
                 <a href="<?php print $application->getURL() . $link; ?>"><?php print $item; ?></a>
               </li>
-            <?php endforeach; ?>
+            <?php endforeach; */ ?>
           </ul>
         </div>
       </div>
